cb24ccf7ea8cc611916e35506ae06f4ae9f36da2
Martin Probst <martin@probst.io>
2013-05-20 13:35:50 +0200

    A bit smarter with the JavaScript.

--- docomplete.js
+++ docomplete.js
@@ -1,9 +1,28 @@
-console.log('Enabling password completion...');
+// Create a no-op object with console's keys or just return console.
+var logger = (function(debug) {
+  if (debug) return console;
+  var logger = {};
+  for (var key in console) {
+    logger[key] = function() {};
+  }
+  return logger;
+})(false); // pass true to debug
 
-var uncompleted = document.querySelectorAll('input[type=password][autocomplete=off]');
-var i = 0;
-for (; i < uncompleted.length; i++) {
-  uncompleted[i].autocomplete = 'on';
+function enableCompletion(selector) {
+  var elements = document.querySelectorAll(selector);
+  var i = 0;
+  for (; i < elements.length; i++) {
+    elements[i].autocomplete = 'on';
+  }
+  return i;
 }
 
-console.log('Done enabling password completion, enabled ' + i);
+logger.time('Enabling password completion');
+
+var completed = 0;
+completed += enableCompletion('input[type=password][autocomplete=off]');
+completed += enableCompletion('form[autocomplete=off]');
+
+logger.log('Done enabling password completion, enabled ' + completed);
+
+logger.timeEnd('Enabling password completion');
